72dfe78a33f7d4a640575f315860f10171261f2f
Daniel Connelly <dhconnelly@gmail.com>
2024-05-15 15:56:49 +0200

    chroot

--- app/main.go
+++ app/main.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
+	"syscall"
 	// Uncomment this block to pass the first stage!
 	// "os"
 	// "os/exec"
@@ -16,25 +19,50 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
-	cmd := exec.Command(command, args...)
-	outPipe, err := cmd.StdoutPipe()
+	// set up chroot directory
+	dir, err := os.MkdirTemp("", "mydocker")
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to create chroot dir: %s", err)
 	}
-	go io.Copy(os.Stdout, outPipe)
-	errPipe, err := cmd.StderrPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-	go io.Copy(os.Stderr, errPipe)
 
-	if err = cmd.Start(); err != nil {
-		log.Fatal(err)
+	// copy the binary to the chroot
+	originalBin, err := os.Open(command)
+	if err != nil {
+		log.Fatalf("failed to open original binary: %s", err)
 	}
+	originalStat, err := originalBin.Stat()
+	if err != nil {
+		log.Fatalf("failed to stat original binary: %s", err)
+	}
+	relCommand := filepath.Base(command)
+	targetBin, err := os.OpenFile(
+		path.Join(dir, relCommand),
+		os.O_CREATE|os.O_TRUNC|os.O_WRONLY,
+		originalStat.Mode())
+	if err != nil {
+		log.Fatalf("failed to create new target binary: %s", err)
+	}
+	if _, err = io.Copy(targetBin, originalBin); err != nil {
+		log.Fatalf("failed to copy binary: %s", err)
+	}
+	originalBin.Close()
+	targetBin.Close()
+
+	// chroot
+	syscall.Chroot(dir)
+
+	// run the program in the chroot
+	chrootCommand := filepath.Join("/", relCommand)
+	cmd := exec.Command(chrootCommand, args...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// exit
 	var exitError *exec.ExitError
-	if err = cmd.Wait(); errors.As(err, &exitError) {
+	if err := cmd.Run(); errors.As(err, &exitError) {
 		os.Exit(exitError.ExitCode())
 	} else if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to run %s in chroot dir %s: %s", chrootCommand, dir, err)
 	}
 }
