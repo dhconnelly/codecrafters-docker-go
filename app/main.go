5383a8d49756527accbba1beae2c88a18adab65a
Daniel Connelly <dhconnelly@gmail.com>
2024-05-16 11:31:33 +0200

    fetch image and apply layers

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -9,22 +10,90 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"path"
 	"path/filepath"
 	"strings"
 	"syscall"
+
+	"github.com/codeclysm/extract"
 )
 
 type AuthResponse struct {
 	Token string `json:"token"`
 }
 
-type FsLayer struct {
-	BlobSum string `json:"blobSum"`
+type Layer struct {
+	MediaType string `json:"mediaType"`
+	Digest    string `json:"digest"`
+	Size      int64  `json:"size"`
 }
 
-type ManifestResponse struct {
-	FsLayers []FsLayer `json:"fsLayers"`
+type Manifest struct {
+	MediaType string  `json:"mediaType"`
+	Layers    []Layer `json:"layers"`
+}
+
+func copyFile(toPath, fromPath string) error {
+	fromF, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer fromF.Close()
+	originalStat, err := fromF.Stat()
+	if err != nil {
+		return err
+	}
+	if err = os.MkdirAll(filepath.Dir(toPath), 0644); err != nil {
+		return err
+	}
+	toF, err := os.OpenFile(
+		toPath,
+		os.O_CREATE|os.O_TRUNC|os.O_WRONLY,
+		originalStat.Mode())
+	if err != nil {
+		return err
+	}
+	defer toF.Close()
+	_, err = io.Copy(toF, fromF)
+	return err
+}
+
+func stripExtensions(path string) string {
+	for {
+		ext := filepath.Ext(path)
+		if len(ext) == 0 {
+			break
+		}
+		cut, ok := strings.CutSuffix(path, ext)
+		if !ok {
+			break
+		}
+		path = cut
+	}
+	return path
+}
+
+type dockerExtractor struct{}
+
+func (dockerExtractor) Link(oldname, newname string) error {
+	if _, err := os.Lstat(newname); err == nil {
+		return os.Remove(newname)
+	}
+	return os.Link(oldname, newname)
+}
+
+func (dockerExtractor) MkdirAll(path string, mode os.FileMode) error {
+	return os.MkdirAll(path, mode)
+}
+
+func (dockerExtractor) OpenFile(name string, flag int, perm os.FileMode) (*os.File, error) {
+	return os.OpenFile(name, flag, perm)
+}
+
+func (dockerExtractor) Symlink(oldname, newname string) error {
+	if _, err := os.Lstat(newname); err == nil {
+		return os.Remove(newname)
+	}
+	return os.Symlink(oldname, newname)
 }
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -47,23 +116,26 @@ func main() {
 	}
 	token := auth.Token
 
-	// fetch manifest
+	// fetch the manifest
 	manifestURL := fmt.Sprintf("https://registry-1.docker.io/v2/library/%s/manifests/%s", name, tag)
 	manifestReq, err := http.NewRequest("GET", manifestURL, nil)
 	if err != nil {
 		log.Fatalf("failed to make manifest request: %s", err)
 	}
 	manifestReq.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	manifestReq.Header.Add("Accept", "application/vnd.docker.distribution.manifest.v2+json")
 	manifestResp, err := http.DefaultClient.Do(manifestReq)
 	if err != nil {
 		log.Fatalf("failed to fetch manifest: %s", err)
 	}
 	manifestDecoder := json.NewDecoder(manifestResp.Body)
-	var manifest ManifestResponse
+	var manifest Manifest
 	if err = manifestDecoder.Decode(&manifest); err != nil {
 		log.Fatalf("failed to decode manifest response: %s", err)
 	}
-	layers := manifest.FsLayers
+	if manifest.MediaType != "application/vnd.docker.distribution.manifest.v2+json" {
+		log.Fatalf("only docker manifest v2 supported for now, found %s", manifest.MediaType)
+	}
 
 	// set up chroot directory
 	dir, err := os.MkdirTemp("", "mydocker")
@@ -73,10 +145,10 @@ func main() {
 
 	// fetch layers
 	var layerPaths []string
-	for _, layer := range layers {
+	for _, layer := range manifest.Layers {
 		layerURL := fmt.Sprintf(
 			"https://registry-1.docker.io/v2/library/%s/blobs/%s",
-			name, layer.BlobSum)
+			name, layer.Digest)
 		layerReq, err := http.NewRequest("GET", layerURL, nil)
 		if err != nil {
 			log.Fatalf("failed to make layer request: %s", err)
@@ -86,7 +158,7 @@ func main() {
 		if err != nil {
 			log.Fatalf("failed to fetch layer: %s", err)
 		}
-		f, err := os.CreateTemp(dir, "layer")
+		f, err := os.CreateTemp(dir, "layer-*.tar.gz")
 		if err != nil {
 			log.Fatalf("failed to create layer file: %s", err)
 		}
@@ -97,36 +169,27 @@ func main() {
 		f.Close()
 	}
 
-	fmt.Printf("saved layer files: %s\n", layerPaths)
+	// extract each layer in order
+	extractor := extract.Extractor{FS: dockerExtractor{}}
+	for _, layerPath := range layerPaths {
+		f, err := os.Open(layerPath)
+		if err != nil {
+			log.Fatalf("failed to open layer: %s", err)
+		}
+		if err = extractor.Gz(context.Background(), f, dir, nil); err != nil {
+			log.Fatalf("failed to gunzip layer: %s", err)
+		}
+		f.Close()
+		if err = os.Remove(layerPath); err != nil {
+			log.Fatalf("failed to remove layer file: %s", err)
+		}
+	}
 
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
-	// copy the binary to the chroot
-	originalBin, err := os.Open(command)
-	if err != nil {
-		log.Fatalf("failed to open original binary: %s", err)
-	}
-	originalStat, err := originalBin.Stat()
-	if err != nil {
-		log.Fatalf("failed to stat original binary: %s", err)
-	}
-	relCommand := filepath.Base(command)
-	targetBin, err := os.OpenFile(
-		path.Join(dir, relCommand),
-		os.O_CREATE|os.O_TRUNC|os.O_WRONLY,
-		originalStat.Mode())
-	if err != nil {
-		log.Fatalf("failed to create new target binary: %s", err)
-	}
-	if _, err = io.Copy(targetBin, originalBin); err != nil {
-		log.Fatalf("failed to copy binary: %s", err)
-	}
-	originalBin.Close()
-	targetBin.Close()
-
 	// run the program in the chroot in a new namespace
-	chrootCommand := filepath.Join("/", relCommand)
+	chrootCommand := filepath.Join("/", command)
 	cmd := exec.Command(chrootCommand, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
--- go.mod
+++ go.mod
@@ -9,3 +9,12 @@
 module github.com/codecrafters-io/docker-starter-go
 
 go 1.22.0
+
+require (
+	github.com/codeclysm/extract v2.2.0+incompatible // indirect
+	github.com/codeclysm/extract/v3 v3.1.1 // indirect
+	github.com/h2non/filetype v1.1.3 // indirect
+	github.com/juju/errors v0.0.0-20181118221551-089d3ea4e4d5 // indirect
+	github.com/klauspost/compress v1.15.13 // indirect
+	github.com/ulikunitz/xz v0.5.11 // indirect
+)
